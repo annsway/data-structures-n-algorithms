209e7e0ef654158c833484e16261d79a50aaf262
Yun Zhou <zhou0360@umn.edu>
2020-03-18 12:43:43 -0500

    Added notes and print out the cards on table.

--- StackCardGame.java
+++ StackCardGame.java
@@ -7,13 +7,20 @@ public class StackCardGame {
 
     public static void main(String[] args){
 
+        // Card game rules:
+        // 1. Player A plays a card, player B plays a card, till there's no card on hand
+        // 2. If there a card with the same number already on the table,
+        //      then the player will collect all the cards behind that card on the table
+        //      including the card she just played, excluding that card on the table
+        // 3. The game stops whenever a player has no cards on hand
+
         Stack<Integer> cardStack = new Stack<>(); // the card stack on the table
         Queue<Integer> playerA = new LinkedList<>();
         Queue<Integer> playerB = new LinkedList<>();
 
-        int mark[] = new int[10]; // a total of 10 cards will be played
+        int mark[] = new int[10]; // this is the key to mark whether or not a card is on the table
 
-        System.out.println("Please enter the cards on players' hands: ");
+        System.out.println("Please enter the cards on players' hands (1-9): ");
         Scanner input = new Scanner(System.in);
 
         // deal the cards
@@ -37,19 +44,19 @@ public class StackCardGame {
 
             // when Player loses cards
             if(mark[a] == 0){ // if this card is not on the table
-                playerA.remove(); // give out the card from playerA
+                playerA.remove(); // give out the first element from queue A
                 cardStack.push(a);
                 mark[a] = 1; // mark 'a=1' to indicate it's already shown on the table
             }
             // when Player A wins cards
             else if (mark[a] == 1){
                 playerA.remove(); // remove the first element 'a' from hand
-                playerA.add(a); // add the first 'a' from the table to hand
+                playerA.add(a); // add the 'a' to the queue immediately
 
                 while(cardStack.lastElement()!=a){
-                    mark[cardStack.lastElement()] = 0;
-                    playerA.add(cardStack.lastElement());
-                    cardStack.pop();
+                    mark[cardStack.lastElement()] = 0; // mark as 0 to indicate the card has been taken out from the table
+                    playerA.add(cardStack.lastElement()); // add the card from table to the hands of player A
+                    cardStack.pop(); // remove the card from the table
                 }
             }
 
@@ -73,7 +80,6 @@ public class StackCardGame {
 
             }
 
-
         }
 
         if (playerA.isEmpty()){
@@ -83,13 +89,14 @@ public class StackCardGame {
                 playerB.remove();
             }
         } else {
-            System.out.println("A wins! \nThe cards on A's hands' are: ");
+            System.out.print("A wins! \nThe cards on A's hands' are: ");
             while(!playerA.isEmpty()){
                 System.out.print(playerA.peek()+" ");
                 playerA.remove();
             }
         }
 
+        System.out.print("\nThe cards on the table are: "+cardStack);
 
     }
 
