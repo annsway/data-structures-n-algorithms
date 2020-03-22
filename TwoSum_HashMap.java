70b8fc900adecc30cb35fd79d849288cf5752950
Yun Zhou <zhou0360@umn.edu>
2020-03-21 21:54:18 -0500

    Initial Commit for TwoSum_HashMap.java

---
+++ TwoSum_HashMap.java
@@ -0,0 +1,38 @@
+import java.util.HashMap; // import the HashMap class
+
+class TwoSum_HashMap {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int remainder;
+        int[] solution = new int[2];
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+
+            remainder = target - nums[i];
+
+            if (map.get(remainder)!=null){
+                solution[0] = map.get(remainder);
+                solution[1] = i;
+
+            }
+
+            map.put(nums[i], i);
+
+        }
+
+        return solution;
+
+    }
+
+    public static void main (String[] args){
+
+        int[] input = {2, 7, 11, 15};
+
+        twoSum(input, 9);
+
+    }
+
+}
