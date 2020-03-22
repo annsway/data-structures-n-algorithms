7be6fc0a757f5e8b9d3172e85751f302856eee3a
Yun Zhou <zhou0360@umn.edu>
2020-03-21 22:14:10 -0500

    Added more methods to the HashMapExample.java file

--- HashMapExample.java
+++ HashMapExample.java
@@ -5,18 +5,37 @@ public class HashMapExample {
 
     public static void main (String[] args){
 
-        HashMap<String, String> gradeMap = new HashMap<>();
-        gradeMap.put("Ann", "A");
-        gradeMap.put("Bob", "B");
-        gradeMap.put("Collin", "C");
+        // Keys and values in a HashMap are actually objects.
+        // To use other types, such as int, you must specify an equivalent wrapper class: Integer. 
+        // For other primitive types, use: Boolean for boolean, Character for char, Double for double
+        HashMap<String, Integer> gradeMap = new HashMap<>();
+
+        // add elements to hash map 
+        gradeMap.put("Ann", 100);
+        gradeMap.put("Bob", 99);
+        gradeMap.put("Collin", 89);
 
         // put the keys of a map to a set
         Set<String> keys = gradeMap.keySet();
 
+        // loop through a hash map 
         for(String key:keys){
             System.out.println(key + "'s grade is "+gradeMap.get(key));
         }
 
+        // or
+        for(String i:gradeMap.keySet()){
+            System.out.println("key: " + i + ", value: "+gradeMap.get(i));
+        }
+
+        // access an item 
+        gradeMap.get("Ann");
+
+        // remove an item 
+        gradeMap.remove("Bob");
+
+        // HashMap size
+        System.out.println("The size of the grade Hash Map is "+gradeMap.size());
 
     }
 
