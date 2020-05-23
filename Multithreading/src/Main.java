9a3af294c0d5047c5e1a2ec3a1c9282c955ec511
Ann Zhou <bookcooklook@gmail.com>
2020-05-23 13:24:53 -0500

    Added comments in Multithreading assignment.

--- Multithreading/src/Main.java
+++ Multithreading/src/Main.java
@@ -9,62 +9,62 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         // read file to lines
-        String path_name = "1000000 Sales Records.csv";
+        String path_name = "9 Sales Records.csv";
         List<String> lines = new ArrayList<>();
         try
         {
             Path filePath = new File(path_name).toPath();
+            // store all the lines of a file into an arraylist
             lines = Files.readAllLines(filePath, Charset.defaultCharset());
         } catch(Exception e){
             System.out.println(e);
         }
 
-        // create two threads
-        int sz = lines.size();
-        int md = sz/2;
+        // get the start and end indexes when processing lines
+        int size = lines.size();
+        int mid = size/2;
 
         System.out.println("Do you want to proceed with single-threading (S) or multi-threading (M) task? Enter S or M.");
         Scanner scan = new Scanner(System.in);
         String input = scan.next().toLowerCase();
 
+        // set up the timer
         long startTime, endTime;
         startTime = System.nanoTime();
 
-
         if(input.equals("s")){
-            Thread t = new Thread( new Tasks(lines,1, sz, false), "T1" );
+            Thread t = new Thread( new Task(lines,1, size, false), "T1" );
             t.start();
             t.join();
+
+            // calculate the time elapsed for processing single-threading task
             endTime = System.nanoTime();
             long timeElapsed = endTime - startTime;
-            System.out.println("Execution time for single-threading in milliseconds: "+timeElapsed/1000000);
-            System.out.println("Execution time for single-threading in seconds: "+timeElapsed/1000000000);
+            System.out.println("Execution time for single-threading task in milliseconds: "+timeElapsed/1000000);
+            System.out.println("Execution time for single-threading task in seconds: "+timeElapsed/1000000000);
 
         } else if(input.equals("m")){
-            Thread t1 = new Thread( new Tasks(lines,1, md, false), "T1" );
-            Thread t2 = new Thread( new Tasks(lines, md, sz, true), "T2" );
+            Thread t1 = new Thread( new Task(lines,1, mid, false), "T1" );
+            Thread t2 = new Thread( new Task(lines, mid, size, true), "T2" );
             t1.start();
             t2.start();
 
             t1.join();
             t2.join();
 
+            // calculate the time elapsed for processing multi-threading task
             endTime = System.nanoTime();
             long timeElapsed = endTime - startTime;
-            System.out.println("Execution time for multi-threading in milliseconds: "+timeElapsed/1000000);
-            System.out.println("Execution time for multi-threading in seconds: "+timeElapsed/1000000000);
+            System.out.println("Execution time for multi-threading task in milliseconds: "+timeElapsed/1000000);
+            System.out.println("Execution time for multi-threading task in seconds: "+timeElapsed/1000000000);
         }
 
-
         // write data from memory into a file
-        FileOutputStream output = new FileOutputStream("1000000 Sales Records.csv", false);
-        for (int i=0; i<sz; i++) {
+        FileOutputStream output = new FileOutputStream("9 Sales Records.csv", false);
+        for (int i=0; i<size; i++) {
             String line = lines.get(i)+"\n";
             byte line_i_b[]=line.getBytes();
             output.write(line_i_b);
         }
     }
-
-
-
 }
--- Multithreading/src/Task.java
+++ Multithreading/src/Task.java
@@ -2,21 +2,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Tasks extends Thread {
+public class Task extends Thread {
     private final double tax = 0.065;
     private List<String> lines;
     private int startIdx;
     private int endIdx;
     private boolean reverse_order;
 
-    public Tasks(List<String> lines, int startIdx, int endIdx, boolean reverse_order){
+    public Task(List<String> lines, int startIdx, int endIdx, boolean reverse_order){
         this.lines = lines;
         this.reverse_order = reverse_order;
         if(!this.reverse_order) {
             this.startIdx = startIdx;
             this.endIdx = endIdx;
         } else {
-            this.startIdx = endIdx-1;
+            // to read from end of the file to the middle
+            this.startIdx = endIdx-1; // endIdx is the size of lines; to read the last element of an array: size - 1
             this.endIdx = startIdx-1;
         }
     }
@@ -24,7 +25,7 @@ public class Tasks extends Thread {
     private String processLine(String line) {
         List<String> data = new ArrayList<>(Arrays.asList(line.split(",")));
 
-        double RevenueWithTax = Math.round(Double.parseDouble(data.get(11))*tax*100)/100.0;
+        double revenueTax = Math.round(Double.parseDouble(data.get(11))*tax*100)/100.0;
 
         String new_line=null;
         for(int i=0; i<14;i++){
@@ -34,7 +35,7 @@ public class Tasks extends Thread {
 
         new_line += "0.065";
         new_line += ",";
-        new_line += String.valueOf(RevenueWithTax);
+        new_line += String.valueOf(revenueTax);
 
         return new_line;
     }
@@ -47,13 +48,14 @@ public class Tasks extends Thread {
 
         int i = this.startIdx;
         while (true) {
+            // override the ith line in memory
             lines.set(i, processLine(lines.get(i)));
 
             if(!this.reverse_order) {
                 i++;
-                if(i>=this.endIdx) break;
+                if(i>=this.endIdx) break; // keep processing lines till i reached endIdx
             } else {
-                i--;
+                i--; // when reading from the end of the file
                 if(i<=this.endIdx) break;
             }
             try {
