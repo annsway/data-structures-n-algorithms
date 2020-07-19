b4ed4ba971ab7233941620467296da1fb64c1141
Ann Zhou <bookcooklook@gmail.com>
2020-07-19 16:42:19 -0500

    Initial commit for removeElements LinkedList.java

---
+++ List/removeElementsLL.java
@@ -0,0 +1,28 @@
+/**
+Remove all elements from a linked list of integers that have value val.
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+**/
+
+public class Solution {
+  public ListNode removeElements(ListNode head, int val) {
+    // Write your solution here
+    if(head == null){
+      return head; 
+    }
+    ListNode dummy = new ListNode(0);
+    ListNode prev = dummy;
+    dummy.next = head;  
+
+    while(head != null){
+      if(head.value == val){
+        prev.next = head.next; 
+      } else {
+        prev = head; 
+      }
+      head = head.next; 
+    }
+    return dummy.next; 
+  }
+}
