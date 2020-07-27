0159aa442fea18fd20fc849d393b32b3ab4167c2
Ann Zhou <bookcooklook@gmail.com>
2020-07-27 15:34:14 -0500

    Added more notes to Quick Selection java file.

--- Sort/QuickSelection.java
+++ Sort/QuickSelection.java
@@ -20,17 +20,24 @@ public class Solution {
             res[i] = array[i];
         }
 
+        Arrays.sort(res); // to return the sorted result 
+
         return res;
     }
 
     public void quickSelect(int[] array, int k, int left, int right){
 
-        if(array == null || k == 0){
+        if(array == null || k == 0){ // base case 
             return;
         }
 
         Random rand = new Random();
-        int pivotIndex = left + rand.nextInt(right - left + 1);
+        int pivotIndex = left + rand.nextInt(right - left + 1); // 注需从 left 开始往后
+
+        // ( - , i) < pivot 
+        // [i, j]: 未探索区域
+        // （j, +): > pivot 
+
         int i = left, j = right - 1;
         swap(array, pivotIndex, right);
 
@@ -42,14 +49,16 @@ public class Solution {
                 j--;
             }
         }
-        swap(array, i, right);
+
+        swap(array, i, right); 
+        // i 现在是pivot的index
         
-        int kSorted = i - left + 1; // Q: Why +1? A: Because kSorted is the number of elements sorted, whereas i and left are indicies
+        int kSorted = i - left + 1; // 项数=[(尾数-首数)/公差]+1
 
         if( kSorted == k){
             return;
         } else if ( kSorted < k){
-            quickSelect(array, k - kSorted, i + 1, right);
+            quickSelect(array, k - kSorted, i + 1, right); // 用i, left, right来做边界，而非k, kSorted
         } else { // selected more than k
             quickSelect(array, k, left, i - 1);
         }
